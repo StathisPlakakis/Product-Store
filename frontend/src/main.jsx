d6ff84bcc5521c4749cab4efa50f1109c486dee3
StathisPlakakis <stathisplakorg1997@gmail.com>
2025-01-17 00:35:44 +0200

    Project Completed

---
+++ frontend/src/main.jsx
@@ -0,0 +1,28 @@
+import { extendTheme } from '@chakra-ui/react'
+import * as React from 'react'
+import { ChakraProvider } from '@chakra-ui/react'
+import * as ReactDOM from 'react-dom/client'
+import App from './App'
+import { BrowserRouter } from 'react-router-dom'
+
+const colors = {
+  brand: {
+    900: '#1a365d',
+    800: '#153e75',
+    700: '#2a69ac',
+  },
+}
+
+const theme = extendTheme({ colors })
+
+
+const rootElement = document.getElementById('root')
+ReactDOM.createRoot(rootElement).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <ChakraProvider theme={theme}>
+        <App />
+      </ChakraProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
+)
